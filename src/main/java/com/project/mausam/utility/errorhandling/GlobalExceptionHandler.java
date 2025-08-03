146ae538cb63c026b6266cb39843098740d899da
prashant-kr08 <kr.prashant0330@gmail.com>
2025-08-03 06:28:48 +0530

    Error handling changes and refactor

--- src/main/java/com/project/mausam/utility/errorhandling/GlobalExceptionHandler.java
+++ src/main/java/com/project/mausam/utility/errorhandling/GlobalExceptionHandler.java
@@ -5,8 +5,8 @@ import java.time.LocalDateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.HttpClientErrorException;
@@ -21,8 +21,7 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(InvalidUnitsException.class)
 	public ResponseEntity<?> invalidUnitsExceptionHandler(InvalidUnitsException invalidUnitsException) {
 		logger.error("Error due to invalid units.", invalidUnitsException);
-		MausamErrorResponse mausamErrorResponse = new MausamErrorResponse(LocalDateTime.now(), invalidUnitsException.getMessage(), "Invalid Units code provided. Please choice the value from 0,1,2");
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new MausamApiResponse<>(false, null, mausamErrorResponse));
+		return getErrorResponse(HttpStatus.BAD_REQUEST, invalidUnitsException.getMessage(), "Invalid Units code provided. Please choice the value from 0,1,2");
 	}
 	
 	@ExceptionHandler(HttpClientErrorException.class)
@@ -32,18 +31,25 @@ public class GlobalExceptionHandler {
 		logger.info(String.valueOf(httpClientErrorException.getStatusCode()));
 		logger.info(apiErrorMsg);
 		 if (httpClientErrorException.getStatusCode() == HttpStatus.NOT_FOUND && apiErrorMsg.toLowerCase().contains("city not found")) {
-			 MausamErrorResponse mausamErrorResponse = new MausamErrorResponse(LocalDateTime.now(), "City not found.", "Please check the city name and try again.");
-			return ResponseEntity.badRequest().body(new MausamApiResponse<>(false, null, mausamErrorResponse));
+			return getErrorResponse(HttpStatus.BAD_REQUEST, "City not found.", "Please check the city name and try again.");
 		}
-		MausamErrorResponse mausamErrorResponse = new MausamErrorResponse(LocalDateTime.now(), "Some error occured.", "Please contact support.");
-		return new ResponseEntity<>(new MausamApiResponse<>(false, null, mausamErrorResponse), HttpStatus.INTERNAL_SERVER_ERROR);
+		 return getErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "Some error occured.", "Please contact support.");
 	}
 	
 	@ExceptionHandler(Exception.class)
-	public ResponseEntity<?> genericExceptionHandler(Exception exception) {
+	public ResponseEntity<?> genericExceptionHandler(final Exception exception) {
 		logger.error("Some Error Occured in system.", exception);
-		MausamErrorResponse mausamErrorResponse = new MausamErrorResponse(LocalDateTime.now(), "Some error occured", "Please contact support.");
-		return new ResponseEntity<>(new MausamApiResponse<>(false, null, mausamErrorResponse), HttpStatus.INTERNAL_SERVER_ERROR);
+		return getErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "Some error occured", "Please contact support.");
 	}
-	
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<?> methodArgumentNotValidExceptionHandler(final MethodArgumentNotValidException methodArgumentNotValidException) {
+		logger.error("Some Error Occured in system.", methodArgumentNotValidException);
+		return getErrorResponse(HttpStatus.BAD_REQUEST, "Validation Error.", "Please check the request and try again.");
+	}
+
+	private ResponseEntity<?> getErrorResponse(final HttpStatus status, final String message, final String details) {
+		MausamErrorResponse mausamErrorResponse = new MausamErrorResponse(LocalDateTime.now(), message, details);
+		return new ResponseEntity<>(new MausamApiResponse<>(false, null, mausamErrorResponse), status);
+	}
 }
