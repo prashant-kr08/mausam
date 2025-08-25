e2ba08d81b1f4ad483391717c66d8371808cbd87
prashant-kr08 <kr.prashant0330@gmail.com>
2025-08-25 22:47:57 +0530

    implement weather stack api

--- src/main/java/com/project/mausam/utility/WeatherApisWindSpeedUnit.java
+++ src/main/java/com/project/mausam/utility/WeatherApisWindSpeedUnit.java
@@ -1,32 +1,46 @@
-package com.project.mausam.provider.openweather.dto;
+package com.project.mausam.utility;
 
-public enum OpenWeatherWindSpeedEnum {
-	STANDARD(0, "meter/sec"),
-	METRIC(1, "meter/sec"),
-	IMPERIAL(2, "miles/hour");
+public enum WeatherApisWindSpeedUnit {
+	KELVIN(0, "meter/sec", "kilometer/hour"),
+	CELSIUS(1, "meter/sec", "kilometer/hour"),
+	FAHRENHEIT(2, "miles/hour", "miles/hour");
 	
 	int unitsId;
-	String windSpeedUnit;
-	
-	OpenWeatherWindSpeedEnum(final int unitsCode, final String windSpeedUnit) {
+	String openWeatherWindSpeedUnit;
+	String weatherStackWindSpeedUnit;
+
+	WeatherApisWindSpeedUnit(final int unitsCode, final String openWeatherWindSpeedUnit, final String weatherStackWindSpeedUnit) {
 		this.unitsId = unitsCode;
-		this.windSpeedUnit = windSpeedUnit;
+		this.openWeatherWindSpeedUnit = openWeatherWindSpeedUnit;
+		this.weatherStackWindSpeedUnit = weatherStackWindSpeedUnit;
 	}
 
 	public int getUnitsId() {
 		return unitsId;
 	}
 
-	public String getWindSpeedUnit() {
-		return windSpeedUnit;
+	public String getOpenWeatherWindSpeedUnit() {
+		return openWeatherWindSpeedUnit;
 	}
 
-	public static String getWindSpeedUnitfromUnitsId(final int id) {
-		for(OpenWeatherWindSpeedEnum windSpeedEnum : values()) {
-			if(windSpeedEnum.getUnitsId() == id) {
-				return windSpeedEnum.getWindSpeedUnit();
+	public String getWeatherStackWindSpeedUnit() {
+		return weatherStackWindSpeedUnit;
+	}
+	
+	public static String getOpenWeatherWindSpeedUnitfromUnitsId(final int id) {
+		for(WeatherApisWindSpeedUnit weatherApisWindSpeedEnum : values()) {
+			if(weatherApisWindSpeedEnum.getUnitsId() == id) {
+				return weatherApisWindSpeedEnum.getOpenWeatherWindSpeedUnit();
 			}
 		}
-		return STANDARD.getWindSpeedUnit();
+		return KELVIN.getOpenWeatherWindSpeedUnit();
+	}
+	public static String getWeatherStackWindSpeedUnitfromUnitsId(final int id) {
+		for(WeatherApisWindSpeedUnit weatherApisWindSpeedEnum : values()) {
+			if(weatherApisWindSpeedEnum.getUnitsId() == id) {
+				return weatherApisWindSpeedEnum.getWeatherStackWindSpeedUnit();
+			}
+		}
+		return KELVIN.getOpenWeatherWindSpeedUnit();
 	}
 }
